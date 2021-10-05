090db2ca5756d861ce13a9311ee3666ea198c47d
Kamlesh <singadkamlesh1112@gmail.com>
2021-10-05 23:44:58 +0530

    Third Commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indiansseo_db',
+        'USER': 'postgress',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
